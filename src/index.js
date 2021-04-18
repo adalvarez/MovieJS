57bb14628854710246dfe3e57e9feff49ca43e64
Adrián Álvarez Calderón <aalvarez@outcoding.com>
2021-04-17 18:59:53 -0600

    Adding secure endpoint for The Movie DB.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import { render } from "react-dom";
 import App from "./App";
 import theMovieDb from "themoviedb-javascript-library";
 theMovieDb.common.api_key = process.env.REACT_APP_MOVIE_KEY;
+theMovieDb.common.base_uri = "https://api.themoviedb.org/3/";
 
 render(<App />, document.getElementById("root"));
